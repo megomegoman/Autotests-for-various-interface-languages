d1e4b75af486530c8c5a36e468f0398585eb8915
Dmitriy Rykov <dimon.man.18@gmail.com>
2022-08-15 23:17:56 +0300

    add "conftest.py"

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoptions('--language', action='store', default="ru", help="Choose interface language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': browser_language})
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
+    yield browser
+    browser.quit()
